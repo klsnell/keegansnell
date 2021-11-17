81ca1c3eed5c0d6dc31a5dfff8e8853f6ef708b5
Keegan Snell <Klsnell0351@gmail.com>
2021-11-16 21:14:24 -0500

    service /create/get mine/delete endpoints working

--- Desktop/RedBadge Project/HomeOwnerHelp Server/controllers/servicesController.js
+++ Desktop/RedBadge Project/HomeOwnerHelp Server/controllers/servicesController.js
@@ -22,4 +22,40 @@ router.post('/create', validateSession, async (req, res) => {
     }
 });
 
+//get request by user
+router.get("/mine", validateSession, async (req, res) => {
+    let {id} = req.user;
+    try{
+        const userRequest = await ServicesModel.findAll({
+            where:{
+                owner_id: id
+            }
+        });
+        res.status(200).json(userRequest);
+    }catch(err){
+        res.status(500).json({error: err});
+    }
+});
+
+//User delete service request
+
+router.delete('/delete/:id', validateSession, async (req, res) => {
+    const userId = req.user.id;
+    const serviceId = req.params.id
+    try {
+        const query = {
+            where: {
+                id: serviceId,
+                owner_id: userId
+            }
+        }
+        await ServicesModel.destroy(query)
+        res.status(200).json({ message: 'Item has successfully been deleted' })
+    } catch (err) {
+        res.status(500).json({
+            message: 'Failed to delete item'
+        })
+    }
+})
+
 module.exports = router;
